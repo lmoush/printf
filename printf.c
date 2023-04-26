75ef744aec794b7948f14a8ca24ea80bc9103d36
Hicham Lotf <www.hicham2582@gmail.com>
2023-04-26 16:01:13 -0700

    _printf

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+void print_buffer(char buffer[], int *buff_ind);
+/**
+ * _printf - it prints the functions
+ * @format: the format of the function
+ * Return: the characters
+ */
+
+int _printf(const char *format, ...)
+{
+
