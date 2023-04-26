d4ef09a799063e20bcb9283219ea7198a8400e2a
Hicham Lotf <www.hicham2582@gmail.com>
2023-04-25 19:10:19 -0700

    task1

--- hicham.c
+++ hicham.c
@@ -76,3 +76,10 @@ int print_octal(va_list types, char buffer[], int flags, int width, int precisio
  * @size: size
  * Return: num of the characters
  */
+
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789abcdef", buffer, flags, 'x', width, precision, size));
+}
+/**
+ *
